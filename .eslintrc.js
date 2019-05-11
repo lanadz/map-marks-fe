41958c88c11a05c95c1bc5e4f193075fe9899c87
lanadz <svitlana.dzyuban@gmail.com>
2019-05-11 13:31:34 +0800

    Temporarily  disable som a11y rules for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
     "extends": "airbnb",
-
+    "rules": {
+        "jsx-a11y/anchor-is-valid": "off",
+        "jsx-a11y/anchor-has-content": "off",
+        "jsx-a11y/click-events-have-key-events": "off",
+        "jsx-a11y/no-static-element-interactions": "off"
+    },
     parser: 'babel-eslint',
     env: {
         jest: true,
